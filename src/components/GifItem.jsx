50e423fd1c25d0d0b5e8888496947349558c081f
Jherson <jhersonmoises.vidalzegarra@everis.nttdata.com>
2023-05-12 11:12:05 -0500

    commit final

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,7 +1,7 @@
 
 
 export const GifItem = ({title,url}) => {
-    console.log({title,url})
+   //// console.log({title,url})
   return (
     <div className="card">
     <img src={url} alt={title}/>
